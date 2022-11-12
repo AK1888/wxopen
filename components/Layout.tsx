f9dc585e7f8771c409879a076b7087675736be48
AK1888 <50517339+AK1888@users.noreply.github.com>
2022-11-12 17:14:53 +0800

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,7 +14,7 @@ const Layout: FunctionComponent = ({ children }) => {
         />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>⚡️跳出微信-wxopen</title>
+        <title>⚡️跳出微信</title>
       </Head>
       {children}
     </>
